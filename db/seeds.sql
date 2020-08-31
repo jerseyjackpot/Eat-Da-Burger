261f5aaa44a227a8a0d95211352fc94880f22789
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-08-31 02:31:18 -0400

    created db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('salmon');
-INSERT INTO burgers (name) VALUES ('luther');
-INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
-INSERT INTO burgers (name, devoured) VALUES ('chili', true);
-INSERT INTO burgers (name, devoured) VALUES ('patty melt', true);
-INSERT INTO burgers (name) VALUES ('chicken burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Rodeo Burger');
+INSERT INTO burgers (name) VALUES ('Thanksgiving Turkey Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Taylor Ham Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mexican Burger', true);
