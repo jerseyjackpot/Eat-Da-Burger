c2d80e9efd801404f31e7cc8c1fdd8d9b60a0b53
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-08-30 23:32:37 -0400

    added sql database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('salmon');
+INSERT INTO burgers (name) VALUES ('luther');
+INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('chili', true);
+INSERT INTO burgers (name, devoured) VALUES ('patty melt', true);
+INSERT INTO burgers (name) VALUES ('chicken burger');
